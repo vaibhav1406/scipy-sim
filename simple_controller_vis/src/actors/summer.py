79006aa30802cc25c7de585aef1a16e59c9a4f49
Brian Thorne <hardbyte@gmail.com>
2009-11-27 12:09:36 +1300

    Clean up and comments

--- simple_controller_vis/src/actors/summer.py
+++ simple_controller_vis/src/actors/summer.py
@@ -4,37 +4,35 @@ Created on 24/11/2009
 @author: brian
 '''
 import logging
-import numpy
-from Actor import Source, Actor
+import numpy as np
+from Actor import Actor
 
-try:
-    import queue
-except ImportError:
-    import Queue as queue
-    
-import time, random # These are used to test the async
+import Queue as queue
+
+#import time, random # These are or can be used to test the async
 
 class Summer(Actor):
     '''
-    This actor takes two sources (for now) and adds them together at the corresponding tagged time. 
+    This actor takes a list of two or more sources and adds them together at the corresponding tagged time. 
     This has to be used with discrete signals, or at least aligned continuous signals.
     '''
-    
+
     def __init__(self, inputs, output_queue):
         """
         Constructor for a summation block
         """
         Actor.__init__(self, output_queue=output_queue)
         self.inputs = list(inputs)
-        self.futures = numpy.zeros_like(self.inputs)
-        
-        
+        self.futures = np.zeros_like(self.inputs)
+
+
     def process(self):
         """Wait for data from both (all) input queues"""
         logging.debug("Running summer process")
-        
-        objects = [in_queue.get(True) for in_queue in self.inputs]     # this is blocking on each queue in sequence
-                   
+
+        # this is blocking on each queue in sequence
+        objects = [in_queue.get(True) for in_queue in self.inputs]
+
         # We are finished iff all the input objects are None
         if objects.count(None) == len(objects):
             logging.info("We have finished summing the data")
@@ -43,17 +41,28 @@ class Summer(Actor):
             return
         tags = [obj['tag'] for obj in objects]
         values = [obj['value'] for obj in objects]
-        
+
         if tags.count(tags[0]) == len(tags):
             # If all tags are the same we can sum the values and output
             new_value = sum(values)
-            logging.debug("Summer had all the same tagged inputs, summed and sent out: (tag: %2.e, value: %2.e)" % (tags[0], new_value ))
+            logging.debug("Summer received all equally tagged inputs, summed and sent out: (tag: %2.e, value: %2.e)" % (tags[0], new_value))
             data = {
                     "tag": tags[0],
                     "value": new_value
                     }
             self.output_queue.put(data)
-        
+        else:
+            logging.debug("Tags were not all equal... First two tags: %.5e, %.5e" % (tags[0], tags[1]))
+            # Since they are not equal, and the tags are always sequential, the oldest timed tags are NEVER
+            # going to have equivalent values from the buffers that have returned newer tags.
+            # So we sum all the values at the oldest tag value. (0th option)
+            # Alternatively (1) we could discard this time step
+            # (2) We could do some sort of integration, histogram style sum for continuous time systems
+            # My feeling is that a CT sum is going to have to be to different to be implemented it the same actor.
+
+            # With the 0th option there is a major problem when one signal isn't creating the same rate of signals
+            # because the current actor (without director) model only processes after receiving an input from
+            # EVERY input queue. So this would be sub optimal also... 
+
         # if the tags won't be the same -  we store a buffer of future tag/value pairs
         #future = max(tags)
-        
--- simple_controller_vis/src/double_ramp_plot.py
+++ simple_controller_vis/src/double_ramp_plot.py
@@ -3,11 +3,8 @@ Created on 23/11/2009
 
 @author: brian
 '''
-try:
-    import queue
-except ImportError:
-    import Queue as queue
-   
+import Queue as queue
+
 from actors.plotter import Plotter
 
 from actors.ramp import Ramp
@@ -23,25 +20,25 @@ if __name__ == '__main__':
     connection1 = queue.Queue(0)
     connection2 = queue.Queue(0)
     connection3 = queue.Queue(0)
-    
+
     src1 = Ramp(connection1)
     src2 = Ramp(connection2)
     summer = Summer([connection1, connection2], connection3)
     dst = Plotter(connection3)
-    
+
     components = [src1, src2, summer, dst]
-    
+
     logging.info("Starting simulation")
     for component in components:
         component.start()
     logging.debug("Finished starting actors")
-    
+
 
     plt.show()   # The program will stay "running" while this plot is open
-    
+
     src1.join()
     src2.join()
     summer.join()
     dst.join()
-    
+
     logging.debug("Finished running actor")
