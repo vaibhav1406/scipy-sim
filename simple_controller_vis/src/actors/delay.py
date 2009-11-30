f3cb260960d785efb87739407628fac74239da30
Brian Thorne <hardbyte@gmail.com>
2009-11-30 15:13:23 +1300

    Code docs and testing

--- simple_controller_vis/src/actors/delay.py
+++ simple_controller_vis/src/actors/delay.py
@@ -12,26 +12,32 @@ try:
     import queue
 except ImportError:
     import Queue as queue
-    
+
 import time, random # These are used to test the async
 
 class Delay(Actor):
     '''
     This actor takes a source and delays it by an arbitrary amount of time.
     '''
-    
-    def __init__(self, input, out, wait=1.0):
+
+    def __init__(self, input, out, wait=1.0/10):
         """
-        default to delaying the signal by one second.
+        Constructor for a delay block.
+
+        @param input: The input queue to be delayed
+
+        @param out: The output queue that has been delayed
+
+        @param wait: The delay time in seconds. Defaults to delaying
+                     the signal by one tenth of a second.
         """
-        Actor.__init__(self, input_queue=input, output_queue=out)
-        
+        super(Delay, self).__init__(input_queue=input, output_queue=out)
         self.delay = wait
-    
+
     def process(self):
         """Delay the input values by a set amount of time..."""
         logging.debug("Running delay process")
-        
+
         obj = self.input_queue.get(True)     # this is blocking
         if obj is None:
             logging.info("We have finished delaying the data")
@@ -46,4 +52,26 @@ class Delay(Actor):
             }
         self.output_queue.put(data)
         obj = None
-        
+
+import unittest
+
+class DelayTests(unittest.TestCase):
+    def test_basic_delay(self):
+        delay = 1
+        q_in = queue.Queue()
+        q_out = queue.Queue()
+
+        input1 = [{'value': 1, 'tag': i } for i in xrange(100)]
+        expected_output = [{'value':1,'tag': i+1 } for i in xrange(100)]
+
+        block = Delay(q_in, q_out, delay)
+        block.start()
+        [q_in.put(i) for i in input1 + [None]]
+
+        block.join()
+        actual_output = [q_out.get() for i in xrange(100)]
+        [self.assertEquals( actual_output[i], expected_output[i]) for i in xrange(100)]
+        self.assertEquals(None, q_out.get())
+
+if __name__ == "__main__":
+    unittest.main()
--- simple_controller_vis/src/actors/ramp.py
+++ simple_controller_vis/src/actors/ramp.py
@@ -13,35 +13,36 @@ class Ramp(Source):
     '''
     This actor is a ramp source
     '''
-    
-    def __init__(self, out, amplitude=2.0, freq=1.0/30, res=10):
+
+    def __init__(self, out, amplitude=2.0, freq=1.0/30, res=10, simulation_time=120):
         """
         default parameters creates a ramp up to 2 that takes 30 seconds with 10 values per "second"
-        
+
         """
         Actor.__init__(self, output_queue=out)
         self.amplitude = amplitude
         self.frequency = freq
         self.resolution = res
-    
+        self.simulation_time = simulation_time
+
     def process(self):
         """Create the numbers..."""
         logging.debug("Running ramp process")
-        tags = numpy.linspace(0, 120, 120*self.resolution)  # for now just compute 2 minutes of values
-        
-        for tag in tags: 
+        tags = numpy.linspace(0, self.simulation_time, self.simulation_time*self.resolution)  # for now just compute 2 minutes of values
+
+        for tag in tags:
             value = tag * self.frequency * self.amplitude
-            
+
             while value >= self.amplitude:
                 value = value - self.amplitude
-            
+
             data = {
                     "tag": tag,
                     "value": value
                     }
             self.output_queue.put(data)
             #logging.debug("Ramp process added data: (tag: %2.e, value: %.2e)" % (tag, value))
-            time.sleep(random.random() * 0.001)     # Adding a delay so we can see the async
-        logging.debug("Ramp process finished adding all data to queue")    
+            #time.sleep(random.random() * 0.001)     # Adding a delay so we can see the async
+        logging.debug("Ramp process finished adding all data to queue")
         self.stop = True
         self.output_queue.put(None)
