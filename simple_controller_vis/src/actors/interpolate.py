c20bd15349594f8cc646f2a3726769eafd51b00f
Brian Thorne <hardbyte@gmail.com>
2009-12-01 12:46:38 +1300

    Added a bibliography file of the papers I am reading. Minor clean up of Actor and delay actors. Added stubs for derivative and interpolate blocks. Started a glossary of terms file.

---
+++ simple_controller_vis/src/actors/interpolate.py
@@ -0,0 +1,22 @@
+'''
+Created on 1/12/2009
+
+@author: brian
+'''
+
+from Actor import Actor
+import logging
+import unittest
+
+class Interpolator(Actor):
+    pass
+
+
+
+class InterpolateTests(unittest.TestCase):
+    def test_basic_intepolate(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
