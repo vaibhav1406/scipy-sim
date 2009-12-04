6c1f0edc830876372016e65575109591bf56589d
allan <allan@localhost>
2009-12-04 14:57:26 +1300

    Add a discrete-time signal source, and a first cut at a display actor for discrete-time signals.

---
+++ simple_controller_vis/src/dtsin_stem.py
@@ -0,0 +1,44 @@
+'''
+Created on 04/12/2009
+
+@author: allan
+'''
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+from actors.stemmer import Stemmer
+
+from actors.dtsin import DTSin
+import matplotlib.pyplot as plt
+import numpy
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("Logger enabled")
+
+
+def run_dtsin_stem():
+    connection1 = queue.Queue(0)
+
+    src = DTSin(connection1) 
+    dst = Stemmer(connection1)
+
+    components = [src, dst]
+
+    logging.info("Starting simulation")
+    for component in components:
+        component.start()
+    logging.debug("Finished starting actors")
+
+
+    plt.show()   # The program will stay "running" while this plot is open
+
+    for component in components:
+        component.join()
+
+    logging.debug("Finished running actor")
+
+if __name__ == '__main__':
+    run_dtsin_stem()
