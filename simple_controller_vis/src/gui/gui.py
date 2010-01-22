e723112e1a1cff7b068eea8e35a28843c47e387e
Brian Thorne <hardbyte@gmail.com>
2010-01-22 13:14:26 -0800

    Can now preview blocks and then drag them onto the canvas.
    Also there was a weird bug where Tkinter didn't like ctsin.py because of something before the start of the text....

--- simple_controller_vis/src/gui/gui.py
+++ simple_controller_vis/src/gui/gui.py
@@ -67,10 +67,16 @@ class Simulation_Canvas(object):
          "preview": "#0099FF", # a blue
          "selected": "#FFCCCC" # pale weak red
          }
+    
+    size = (width, height) = (550, 250)
+    
+    font = ("Helvetica", 10)
+    
+    
     def __init__(self, frame):
         # Create the canvas
         self.canvas = Canvas(frame, 
-                             width=550, height=250,
+                             width=self.width, height=self.height,
                              relief=RIDGE, 
                              background =self.colours["background"], 
                              borderwidth =1)
@@ -81,26 +87,48 @@ class Simulation_Canvas(object):
         self.canvas.tag_bind ("DRAG", "<Leave>", self.leave)
         self.canvas.pack(side=TOP)
         
-        # Some locations
-        self.PREVIEW_LOCATION = (self.PREVIEW_X, self.PREVIEW_Y) = (50, 30)
-        self.BLOCK_SIZE = (self.BLOCK_WIDTH, self.BLOCK_HEIGHT) = (100, 50)
+        # Some default locations
+        self.PREVIEW_WIDTH = 80
+        self.PREVIEW_LOCATION = (self.PREVIEW_X, self.PREVIEW_Y) = (15, 30)
+        self.BLOCK_SIZE = (self.BLOCK_WIDTH, self.BLOCK_HEIGHT) = (70, 50)
+        
+        # Draw a "preview" area
+        self.canvas.create_line(self.PREVIEW_WIDTH,0,self.PREVIEW_WIDTH,250,dash=True)
         
     def preview_actor(self, codefile):
         """
         Display a preview of an actor or compisite actor in the canvas,
         it will be dragable into desired position
         """
-        logging.debug("Preview of %(name)s on simulation canvas." % {'name':codefile.name})
-        font = ("Helvetica", 14)
-        self.canvas.create_rectangle(*self.PREVIEW_LOCATION + self.BLOCK_SIZE, fill=self.colours["preview"], tags =["DRAG", codefile.name])
-        self.canvas.create_text (*self.PREVIEW_LOCATION, font=font, text = codefile.name, tags =["DRAG", codefile.name])
+        logging.debug("Creating a preview of %(name)s on simulation canvas." % {'name':codefile.name})
+        
+        logging.debug("Delecting any existing items still tagged 'preview'")
+        self.canvas.delete("preview")
+        
+        
+        self.canvas.create_rectangle(*self.PREVIEW_LOCATION + self.BLOCK_SIZE, 
+                                     fill=self.colours["preview"], 
+                                     tags =["DRAG",                     # Anything moveable will have the "DRAG" tag
+                                            "name:" + codefile.name,    # Every blocks name will tagged so we can group bits together
+                                            "type:block",               # type: block/text
+                                            "preview"
+                                            ]
+                                     )
+        self.canvas.create_text (self.PREVIEW_X + (self.BLOCK_WIDTH/2), self.PREVIEW_Y + 25, 
+                                 font=self.font, text = codefile.name, 
+                                 tags =["DRAG", 
+                                        "name:" + codefile.name,
+                                        "type:text",
+                                        "preview"
+                                        ]
+                                 )
     
     def mouse_down(self, event):
+        logging.debug("The mouse was pressed at (%d, %d)" % (event.x, event.y))
         logging.debug("The mouse went down on a block. Binding mouse release...")
-        # TODO: WHAT BLOCK? SELECT IT.
         selected = self.canvas.gettags("current")
         logging.debug("Currently selected items tags are %s" % selected.__repr__())
-        self.selected_name = [a for a in selected if a not in ["current", "DRAG"]][0]
+        self.selected_name = [a for a in selected if a.startswith("name:") ][0]
         logging.debug("Block selected was %s" % self.selected_name)
         self.select_location = (event.x, event.y)
         self.canvas.addtag('Selected', 'withtag', self.selected_name) 
@@ -111,16 +139,23 @@ class Simulation_Canvas(object):
         logging.debug("The mouse was released at (%d, %d)" % (event.x, event.y))
         if event.x >= 0 and event.x <= self.canvas.winfo_width() \
             and event.y >= 0 and event.y <= self.canvas.winfo_height():
-                logging.debug("Valid move")
-                event.widget.itemconfigure("Selected", fill=self.colours["block"])
-                
+                logging.debug("Valid move inside canvas. Relocating block.")
                 self.canvas.move("Selected", event.x - self.select_location[0], event.y - self.select_location[1])
-                #block = self.canvas.find_withtag('Selected')
-                block = self.canvas.gettags("Selected")
-                logging.debug("Block moved was made up of these components: %s" % block.__repr__())
+                if event.x >= self.PREVIEW_WIDTH:
+                    if "preview" in self.canvas.gettags("Selected"):
+                        logging.info("Moved out of preview zone, adding now component to model")
+                        self.canvas.dtag("preview")
+                        #TODO HERE - add to model compiler or what ever...
+                    event.widget.itemconfigure("Selected", fill=self.colours["block"])
+                else:
+                    event.widget.itemconfigure("Selected", fill=self.colours["preview"])    
+                
+                #block = self.canvas.gettags("Selected")
+                #logging.debug("Block moved was made up of these components: %s" % block.__repr__())
                 self.canvas.dtag("Selected", "Selected")
                                 
-        else: logging.debug("Invalid move.") 
+        else: 
+            logging.info("Invalid move.")
         # TODO: Move selected block to here.
         
     def enter(self, event):
