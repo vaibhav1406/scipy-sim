c6c37c03d4098deb3db0b71de2276d3f99fbf930
Brian Thorne <hardbyte@gmail.com>
2010-02-02 19:05:27 -0800

    Added a few more files, still broken everything

---
+++ simple_controller_vis/src/models/actors/string/intparser.py
@@ -0,0 +1,40 @@
+'''
+Created on Feb 2, 2010
+
+@author: brianthorne
+'''
+from models.actors import Siso
+
+class IntParser(Siso):
+    '''
+    Takes a tagged string input and if possible converts into integers.
+    '''
+
+    def __init__(self, input_queue, output_queue, child_handles_output=True):
+        super(IntParser, self).__init__(input_queue=input_queue,
+                                  output_queue=output_queue)
+
+    def siso_process(self, obj):
+        try:
+            obj['value'] = int(obj['value'])
+            self.output_queue.put(obj)
+        except:
+            pass
+        
+import unittest
+from models.actors import Channel
+from models.actors.siso import TestSisoActor
+class TestIntParser(unittest.TestCase):
+    
+    def setUp(self):
+        self.q_in = Channel()
+        self.q_out = Channel()
+    
+    def test_basic(self):
+        inputs = [{'value':str(i), 'tag':i} for i in xrange(10)]
+        expected_outputs = [{'value':int(i), 'tag':i} for i in xrange(10)]
+        block = IntParser(self.q_in, self.q_out)
+        TestSisoActor(self, block, inputs, expected_outputs)
+        
+if __name__ == '__main__':
+    unittest.main()
