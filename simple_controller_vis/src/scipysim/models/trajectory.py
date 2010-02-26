7da5200c601b9354a7db684883b1827092cfa61f
Brian Thorne <hardbyte@gmail.com>
2010-02-26 19:54:04 +1300

    Added a constant acceleration integration example, was going to integrate again but might be a bug between integration threads.

---
+++ simple_controller_vis/src/scipysim/models/trajectory.py
@@ -0,0 +1,42 @@
+"""
+A model based on the bouncing ball example in simulink.
+"""
+from scipysim.actors import Model, MakeChans
+from scipysim.actors.signal import Ramp, Copier
+from scipysim.actors.math import Summer, Constant
+from scipysim.actors.math import CTIntegratorDE1 as Integrator
+from scipysim.actors.display import Plotter
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("Logger enabled")
+
+class ThrownBall(Model):
+    '''
+    A simple example simulation where ...
+    '''
+
+    def __init__(self):
+        '''
+        A basic simulation that ...
+        '''
+        wires = MakeChans(10)
+
+        gravity = -9.81
+        initial_position = 10 # vertical meters
+        initial_velocity = 15 # m/s vertical, up is positive 
+
+        self.components = [
+            Constant(wires[0], value=gravity, resolution=100, simulation_time=4),
+            Integrator(wires[0], wires[1], initial_position),
+            Copier(wires[1], [wires[2], wires[3]]),
+            Plotter(wires[2], title="Velocity", own_fig=False),
+            #Integrator(wires[3], wires[4], initial_velocity),
+            #Copier(wires[4], [wires[5], wires[6]]),
+            #Plotter(wires[4], title="Displacement", own_fig=True),
+
+        ]
+
+
+if __name__ == '__main__':
+    ThrownBall().run()
