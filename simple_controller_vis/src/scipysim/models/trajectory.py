4915f4f01b69cc753d8a8a98c901a106fc3afe50
Brian Thorne <hardbyte@gmail.com>
2010-03-01 17:07:04 +1300

    Misc comments and spelling and titles, labels etc...

--- simple_controller_vis/src/scipysim/models/trajectory.py
+++ simple_controller_vis/src/scipysim/models/trajectory.py
@@ -30,11 +30,10 @@ class ThrownBall(Model):
             Constant(wires[0], value=gravity, resolution=100, simulation_time=4),
             Integrator(wires[0], wires[1], initial_position),
             Copier(wires[1], [wires[2], wires[3]]),
-            Plotter(wires[2], title="Velocity", own_fig=True),
+            Plotter(wires[2], title="Velocity", xlabel="Time (s)", ylabel="(m/s)"),
             Integrator(wires[3], wires[4], initial_velocity),
             Copier(wires[4], [wires[5], wires[6]]),
-            Plotter(wires[5], title="Displacement", own_fig=True),
-
+            Plotter(wires[5], title="Displacement", own_fig=True, xlabel="Time (s)", ylabel="(m)"),
         ]
 
 
