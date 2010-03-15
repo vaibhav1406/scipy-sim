c77cab266954529beaed705fe543f37224cf09e7
Brian Thorne <hardbyte@gmail.com>
2010-03-15 14:51:15 +1300

    Use multiple inheritance to enforce "newstyle" classes for Channel, and deepcopy every item put into a Channel to ensure item immutability between threads.

--- simple_controller_vis/src/scipysim/actors/core/actor.py
+++ simple_controller_vis/src/scipysim/actors/core/actor.py
@@ -7,11 +7,12 @@ Created on 19/11/2009
 import Queue as queue
 import threading
 import logging
+from copy import deepcopy as copy
 
-class InvalidSimulationInput( TypeError ):
+class InvalidSimulationInput(TypeError):
     pass
 
-class Channel( queue.Queue ):
+class Channel(queue.Queue, object):
     '''
     A Channel is a derived class of the Python Queue, used for communicating between Actors.
     
@@ -23,20 +24,31 @@ class Channel( queue.Queue ):
     @param domain: The two letter domain code as a string
     '''
 
-    def __init__( self, domain="CT" ):
+    def __init__(self, domain="CT"):
         '''Construct a queue with type information.
         '''
-        queue.Queue.__init__( self )
+        #queue.Queue.__init__(self)
+        super(Channel, self).__init__()
         self.domain = domain
 
-def MakeChans( num ):
+    def put(self, new_item, *args, **kwargs):
+        '''
+        Put an item into this channel, ensures immutable by copying.
+        '''
+        if new_item is not None:
+            item = copy(new_item)
+        else:
+            item = None
+        super(Channel, self).put(item, args, kwargs)
+
+def MakeChans(num):
     '''Make a list of channels.
     @param num of channels to create
     '''
-    return [Channel() for i in xrange( num )]
+    return [Channel() for i in xrange(num)]
 
 
-class Actor( threading.Thread ):
+class Actor(threading.Thread):
     '''
     This is a base Actor class for use in a simulation.
     All blocks should inherit from this, including composite models.
@@ -46,7 +58,7 @@ class Actor( threading.Thread ):
     domain and number of an Actors inputs and outputs. As a convention 
     None means any number or any domain is allowed.
     
-    Class Atributes:
+    Class Attributes:
     
     num_inputs - Either None or a integer number of how many channels the block takes.
     num_outputs - Same but for output of a block.
@@ -57,10 +69,10 @@ class Actor( threading.Thread ):
     '''
     num_inputs = None
     num_outputs = None
-    output_domains = ( None, )
-    input_domains = ( None, )
+    output_domains = (None,)
+    input_domains = (None,)
 
-    def __init__( self, input_queue=None, output_queue=None, *args, **kwargs ):
+    def __init__(self, input_queue=None, output_queue=None, *args, **kwargs):
         '''Constructor for a generic base actor.
         
         @param input_queue: If an input queue is not passed in, one will be created.
@@ -69,31 +81,31 @@ class Actor( threading.Thread ):
         
         Any other named parameters will be passed on to the Thread constructor.
         '''
-        super( Actor, self ).__init__()
+        super(Actor, self).__init__()
 
 
         # Every actor will have at least an input thread - even if its just a control
         if input_queue is None:
-            input_queue = queue.Queue( 0 )
+            input_queue = queue.Queue(0)
         self.input_queue = input_queue
 
         # A sink doesn't require an output queue so this could be None
         self.output_queue = output_queue
 
         self.stop = False
-        self.setDaemon( True )
+        self.setDaemon(True)
 
 
-    def run( self ):
+    def run(self):
         '''
         Run this actors objects thread
         '''
-        logging.debug( "Started running an actor thread" )
+        logging.debug("Started running an actor thread")
         while not self.stop:
             #logging.debug("Some actor is processing now")
             self.process()
 
-    def process( self ):
+    def process(self):
         '''
         The process function is called as often as possible by the threading 
         or multitasking library. No guarantees are made about timing, or that
@@ -101,12 +113,12 @@ class Actor( threading.Thread ):
         '''
         raise NotImplementedError()
 
-class DisplayActor( Actor ):
+class DisplayActor(Actor):
     '''A display actor is a sink. It also draws to the screen'''
     num_inputs = 1
     num_outputs = 0
 
-class Source( Actor ):
+class Source(Actor):
     '''
     A Source is an abstract interface for a signal source.
     Requires an output queue.
@@ -114,11 +126,11 @@ class Source( Actor ):
     num_inputs = 0
     num_outputs = 1
 
-    def __init__( self, output_queue, simulation_time=None ):
-        super( Source, self ).__init__( output_queue=output_queue )
+    def __init__(self, output_queue, simulation_time=None):
+        super(Source, self).__init__(output_queue=output_queue)
         self.simulation_time = simulation_time
 
-    def process( self ):
+    def process(self):
         '''
         This abstract method gets called in a loop untill the actor sets its stop variable to true
         '''
