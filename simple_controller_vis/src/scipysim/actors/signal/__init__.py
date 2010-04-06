f8a75579969205873ee90270a3701ecbf3181d34
Allan McInnes <Allan McInnes@localhost>
2010-04-06 22:44:54 +1200

    Add an event-filter actor that converts CT signals into discrete-event (or quantized state) signals. Use of this actor is demonstrated in the new qs_sin_plot.py model.

--- simple_controller_vis/src/scipysim/actors/signal/__init__.py
+++ simple_controller_vis/src/scipysim/actors/signal/__init__.py
@@ -2,6 +2,7 @@
 from copier import Copier
 from decimator import Decimator
 from delay import Delay
+from eventfilter import EventFilter
 from interpolator import InterpolatorZero
 from interpolator import InterpolatorStep
 from interpolator import InterpolatorLinear
