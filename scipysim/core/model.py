3b2bc207ebed3b491fbdbf25007bfb4e712621e4
Allan McInnes <Allan McInnes@localhost>
2010-08-26 00:46:58 +1200

    Don't need any plot magic now that plots are in separate processes.

--- scipysim/core/model.py
+++ scipysim/core/model.py
@@ -36,30 +36,11 @@ class Model(Actor):
         '''
         assert hasattr(self, 'components')
 
-        """
-        As this is the "main" thread we will assume control of the GUI
-        until we enter the TK main loop, then plotters can paint their pictures.
-        """
-        need_plot_magic = any(issubclass(a.__class__, DisplayActor) for a in self.components)
-        if False and need_plot_magic:
-            from scipysim.actors.display.plotter import GUI_LOCK
-            GUI_LOCK.acquire(blocking=True)
-
         logging.info("Starting simulation")
         [component.start() for component in self.components]
         logging.debug("Finished starting actors")
 
-        if False and need_plot_magic:
-            import matplotlib.pyplot as plt
-            logging.info('The program will stay "running" while the plot is open')
-
-            GUI_LOCK.notifyAll()
-            GUI_LOCK.release()
-
         [component.join() for component in self.components]
-
-        if False and need_plot_magic:
-            plt.show()
 
         logging.debug("Finished running simulation")
 
