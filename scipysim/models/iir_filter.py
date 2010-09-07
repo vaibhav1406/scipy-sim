7de414b257e9deb5795b7eb237c20bb86265e776
Allan McInnes <Allan McInnes@localhost>
2010-09-07 17:29:20 +1200

    Revise IIR filter example to actually demonstrate low-pass filtering.

--- scipysim/models/iir_filter.py
+++ scipysim/models/iir_filter.py
@@ -19,25 +19,31 @@ class IIR(Model):
 
     def __init__(self):
 
-        wires = MakeChans(10, 'DT')
+        wires = MakeChans(13, 'DT')
 
         # Initial condition
-        wires[7].put(Event(0,0))
+        wires[9].put(Event(0,0))
 
         self.components = [
             # Signal source
             DTSinGenerator(wires[0], simulation_length=200),
-            Split(wires[0], [wires[1], wires[2]]),
-            StemPlotter(wires[1], title="Original signal", refresh_rate=5, xlabel="n", ylabel="value"),
+            DTSinGenerator(wires[1], amplitude = 0.1, freq = 0.45, simulation_length=200),
+            Summer(inputs = [wires[0], wires[1]], output_channel = wires[2]),
 
-            # The filter: y[n] = x[n] - 0.5y[n-1]
-            Summer([wires[2], wires[7]], wires[3]),
-            Split(wires[3], [wires[4], wires[5]]),
-            Delay(wires[4], wires[6], wait=1),  
-            Gain(wires[6], wires[7], gain = -0.5),
+            Split(wires[2], [wires[3], wires[4]]),
+            StemPlotter(wires[3], title="Original signal", refresh_rate=5, xlabel="n", ylabel="value"),
+
+            # The filter: y[n] = x[n] + 0.7x[n-1] + 0.7y[n-1]
+            Summer(inputs = [wires[4], wires[10]], output_channel = wires[5]),
+            Split(wires[5], [wires[6], wires[7]]),
+            Delay(wires[6], wires[8], wait=1),
+            Gain(wires[8], wires[9], gain = 0.7),
+            Split(wires[9], [wires[10], wires[11]]),
+
+            Summer(inputs = [wires[7], wires[11]], output_channel = wires[12]),
 
             # Plot
-            StemPlotter(wires[5], title="Filtered signal", refresh_rate=5, xlabel="n", ylabel="value"),
+            StemPlotter(wires[12], title="Filtered signal", refresh_rate=5, xlabel="n", ylabel="value"),
         ]
 
 
