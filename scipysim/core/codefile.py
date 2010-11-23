50aa125276a44d6435ffd90ad6de631f85b4637d
Brian Thorne <BThorne@dynamiccontrols.com>
2010-11-23 15:01:56 +1300

    Add parameter support to CodeFile

--- scipysim/core/codefile.py
+++ scipysim/core/codefile.py
@@ -2,6 +2,18 @@
 Created on Jan 18, 2010
 
 @author: brianthorne
+
+TODO: Add GUI support. image, position, input connectors etc...
+'''
+
+'''
+These should both work:
+    from scipysim.actors import sin
+or 
+    import scipysim.actors.sin
+
+TODO CHECK: If the filename is an absolute path this might not import 
+properly because the Actor or Model may inherit from other files?
 '''
 
 from os import path
@@ -20,58 +32,66 @@ from event import Event
 from siso import Siso
 from actor import Actor, Source
 
-class CodeFile:
+class CodeFile(object):
     """
-    This class wraps an Actor or a model for the GUI.
-    It contains instructions for drawing, connecting and running a simulation.
+    Wrapper around code defining an *Actor* or *Model*.
     
+    CodeFile instances exist for every source module containing Actors
+    and keep track of the path to the source as well as the actual class.
     """
     def __init__(self, filepath, name=None):
-        """Wrap an Actor or Model file, parses the number of inputs etc.
-        If the name is not given it will be the stripped file name.
+        """Wraps an Actor found in the Python file at the given path.
+
+        Parses the Actor for the number of inputs, outputs and parameters.
         
-        TODO: Add GUI support. image, position, input connectors etc...
+        If the name of the Actor is not given this will attempt to use the
+        stripped file name.
+
+        Params
+        ======
+
+        filepath
+            The relative (or absolute) path to the python file containing
+            the *Actor* to load.
+
+        name
+            The name of the *Actor* class within the given file.
+
         """
+        logging.debug("Trying to load a '%s' Actor from module found at '%s'" % (name, filepath))
+
         self.filepath = filepath
         assert path.exists(filepath)
-        self.name = path.split(filepath)[1].split('.')[0] if name is None else name
-        self.image = None
-        logging.debug("Trying to load a '%s' block from '%s'" % (self.name, self.filepath))
-
-        '''
-        We can use a import statement like: 
-            from scipysim.actors import sin
-        or 
-            import scipysim.actors.sin
-        but not 
-            import sin
-        unless sin was in the top directory or on the python path. 
-        If the filename is an absolute path this might not import 
-        properly because the Actor or Model may inherit from other files.
         
-        Maybe searching for 'scipysim' in the file path, and if we find it
-        strip out everything before it. But then if users want to have
-        models somewhere else this wouldn't work.
-        '''
-        the_path = path.split(filepath)[0]
+        the_path, the_file = path.split(filepath)
+        self.module_name, ext = path.splitext(the_file)
+        assert 'py' in ext
+        
         logging.debug("Adding '%s' to the Python Path" % the_path)
         sys.path.insert(0, the_path)
         
-        logging.debug('Now trying to execute `import %s` to get the module containing the Class.' % self.name)
-        module = __import__(self.name, level=10)
-        logging.debug("'%s' module imported" % self.name)
+        logging.debug('Now trying to execute `import %s` to get the module containing the Class.' % self.module_name)
+        module = __import__(self.module_name, level=10)
+        logging.debug("'%s' module imported" % module)
 
+        # Dynamically find the class that is not a unittest or part of the core
+        # inside "module" - ideally we have been given its name!
 
-        # TODO: we need to dynamically find the class that is not a unittest
-        # inside "module"
+        # If the Actor name wasn't given, set it to the name of the module
+        self.name = self.module_name.title() if name is None else name
+        
         try:
-            # Hopefully the module contains a class with the same name as the module
-            # TODO: Fix for camelcase eg DTSinGenerator
-            block_class = getattr(module, self.name.title())
+            # Hopefully the module contains a class with the same name as given
+            block_class = getattr(module, self.name)
         except AttributeError:
+            # If the name was given and an AttributeError was caught the user has
+            # given us a bad name.
+            if name is not None:
+                raise NameError('Class "%s" was not found in module "%s".' % (self.name, self.module_name))
             # otherwise we have to try harder...
             logging.debug("Module was not called the same as the filename")
             block_class = None
+        # TODO: Refactor this into a function
         if block_class is None:
             logging.debug(interrogate(module))
 
@@ -98,7 +118,9 @@ class CodeFile:
             logging.debug('Modules found that nearly match module name are: %s.' % modules)
             module_name = modules[0]
             block_class = getattr(module, module_name)
-        logging.debug("Block class is '%s'" % block_class)
+            self.name = module_name
+        logging.debug("Actor's class is '%s'" % block_class)
+
         # Find out how many input and output channels the block can take.
         if issubclass(block_class, Siso):
             logging.debug("Inherits from SISO - we know that it has one input and one output")
@@ -118,17 +140,57 @@ class CodeFile:
         self.output_domains = block_class.output_domains
         self.input_domains = block_class.input_domains
 
+        # Save the class itself in two ways, as .block_class and as .ClassName
+        setattr(self, self.name, block_class)
+        self.block_class = block_class
+
+    def get_default_parameters(self):
+        '''
+        Return a dictionary mapping parameter names to default parameters, if
+        there is no default parameter, None is used.
+        '''
+        if hasattr(self, 'params'): return self.params
+        logging.debug('Loading the parameters and defaults for the %s Actor' % self.name)
+        self.arg_spec = inspect.getargspec(self.block_class.__init__)
+
+        
+        # Iterate over the args list, e.g.,
+        #  ['self','input_channel','refresh_rate','title','own_fig','xlabel','ylabel']
+        # adding each argument name to our dictionary. Then iterate in reverse over
+        # the defaults tuple to match up the default args with the arg names.
+        
+        parameters = {}
+        for arg_name in self.arg_spec.args:
+            if arg_name is not "self":
+                parameters[arg_name] = None
+        defaults = [None] if self.arg_spec.defaults is None else self.arg_spec.defaults
+        for default_param, arg_name in zip(reversed(defaults), reversed(self.arg_spec.args)):
+            parameters[arg_name] = default_param
+
+        self.params = parameters
+        return parameters
+
     def get_code(self):
-        """Load an actor or model file."""
+        '''Load an actor or model file.
+
+        TODO: Use inspect.getsource
+        '''
         text = "".join(open(self.filepath, 'r').readlines())
         return text
 
     def __repr__(self):
-        '''Return the name of this code file object'''
-        return self.name
-        '''return "<%(name)s - %(inputs)d inputs, %(outputs)d outputs >" % {
+        '''
+        Create a string representation of the code file.
+        TODO:
+            ----------------------------------------
+            |           Block Name                 |
+           <| Input name (dt)  |  Output name (ct) |>
+           <|     ...          |                   |
+            ----------------------------------------
+        '''
+        return '''<%(name)s - %(inputs)s inputs, %(outputs)s outputs >''' % {
                 "name": self.name,
                 "inputs": self.num_inputs,
                 "outputs": self.num_outputs
-            }'''
+            }
 
--- scipysim/core/test_codefile.py
+++ scipysim/core/test_codefile.py
@@ -30,17 +30,29 @@ class Test_Code_File_Path( unittest.TestCase ):
     def test_file_is_class_name( self ):
         '''Test that a class with the same name as the filename makes valid CodeFile'''
         c = CodeFile( path.join( PATH_TO_SRC_DIR, 'scipysim', 'actors', 'math', 'trig', 'sin.py' ) )
+        self.assertEquals(c.name, "Sin")
+
+    def test_file_when_given_class_name( self ):
+        filepath = path.join( PATH_TO_SRC_DIR, 'scipysim', 'actors', 'strings', 'intparser.py' )
+        c = CodeFile( filepath, "IntParser" )
+        self.assertEquals(c.name, "IntParser")
+
+    def test_fail_on_invalid_name( self ):
+        '''Test that an exception is raised when loading a non-existant class from a file.'''
+        filepath = path.join( PATH_TO_SRC_DIR, 'scipysim', 'actors', 'strings', 'intparser.py' )
+        self.assertRaises(NameError, CodeFile,  filepath, "Parser" )  # Parser is not defined in intparser.py
+        
 
     def test_file_isnt_class_name( self ):
         '''Test that a class in a trickily named file is still parsed as a valid CodeFile'''
         c = CodeFile( path.join( PATH_TO_SRC_DIR, 'scipysim', 'actors', 'strings', 'intparser.py' ) )
+        self.assertEquals(c.name, "IntParser")
 
 class Test_Code_File_Num_Input_Output_Parsing( unittest.TestCase ):
-    src_dir = PATH_TO_SRC_DIR
 
     def test_siso_code_file( self ):
         '''Test by loading a siso actor from a hardcoded path'''
-        filepath = path.abspath(path.join(self.src_dir, 'scipysim', 'actors', 'math', 'trig', 'sin.py'))
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim', 'actors', 'math', 'trig', 'sin.py'))
         c = CodeFile( filepath )
         self.assertNotEqual(None, c)
         self.assertEqual( c.num_inputs, 1 )
@@ -48,36 +60,60 @@ class Test_Code_File_Num_Input_Output_Parsing( unittest.TestCase ):
 
     def test_display_actor( self ):
         '''Test that a plotter has one channel input'''
-        c = CodeFile( self.src_dir + '/scipysim/actors/display/stemmer.py' )
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim','actors','display','stemmer.py'))
+        c = CodeFile( filepath  )
         self.assertEqual( c.num_inputs, 1 )
         self.assertEqual( c.num_outputs, 0 )
 
     def test_dynamic_actor( self ):
         '''Test that a block that can have multiple inputs (summer)'''
-        c = CodeFile( self.src_dir + '/scipysim/actors/math/summer.py' )
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim','actors', 'math', 'summer.py'))
+        c = CodeFile( filepath )
         self.assertEqual( c.num_outputs, 1 )
         self.assertEqual( c.num_inputs, None )
 
     def test_composite_model( self ):
         '''Test a composite_actor for num of inputs and output channels'''
-        c = CodeFile( self.src_dir + '/scipysim/actors/math/trig/DTSinGenerator.py' )
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim','actors','math','trig','DTSinGenerator.py'))
+        c = CodeFile( filepath )
         self.assertEqual( c.num_outputs, 1 )
         self.assertEqual( c.num_inputs, 0 )
 
 class Test_Code_File_Input_Output_Domain_Parsing( unittest.TestCase ):
-    src_dir = PATH_TO_SRC_DIR
-
 
     def test_dynamic_actor( self ):
         '''Test the a block that can have multiple inputs (summer)'''
-        c = CodeFile( self.src_dir + '/scipysim/actors/math/summer.py' )
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim','actors', 'math', 'summer.py'))
+        c = CodeFile( filepath )
         self.assertTrue ( all( ( domain == None for domain in c.output_domains ) ) )
 
     def test_composite_model( self ):
         '''Test a composite_actor for num of inputs and output channels'''
-        c = CodeFile( self.src_dir + '/scipysim/actors/math/trig/DTSinGenerator.py' )
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim','actors','math','trig','DTSinGenerator.py'))
+        c = CodeFile( filepath )
         self.assertTrue( all( ( domain == "DT" for domain in c.output_domains ) ) )
 
+class Test_Code_File_Parameters( unittest.TestCase ):
+
+    def test_actor_parameter_names( self ):
+        '''Check the parameters dictionary contains the correct names'''
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim','actors','math','trig','DTSinGenerator.py'))
+        c = CodeFile( filepath )
+        # arguments should contain this:
+        # self, out, amplitude=1.0, freq=0.01, phi=0.0, simulation_length=100
+        params = c.get_default_parameters()
+        for name in ['out', 'amplitude', 'freq', 'phi', 'simulation_length']:
+            self.assertTrue(name in params)
+
+    def test_actor_parameter_defaults(self):
+        '''Check the parameters dictionary contains the correct default arguments'''
+        filepath = path.abspath(path.join(PATH_TO_SRC_DIR, 'scipysim','actors','math','trig','DTSinGenerator.py'))
+        c = CodeFile( filepath )
+        # arguments should contain this:
+        # self, out, amplitude=1.0, freq=0.01, phi=0.0, simulation_length=100
+        params = c.get_default_parameters()
+        for name, value in [('out', None), ('amplitude',1.0), ('freq',0.01), ('phi',0.0), ('simulation_length',100)]:
+            self.assertEquals(params[name], value)
 
 if __name__ == "__main__":
     unittest.main()
